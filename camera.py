145c97d52faf62c9ba791625d47c34343eb05f34
yuppyboy <calebharris1225@gmail.com>
2022-01-22 15:48:35 -0500

    m

--- camera.py
+++ camera.py
@@ -1,18 +1,11 @@
-from keras.models import model_from_json
-from keras.preprocessing import image as im
-
 import cv2
 import classifier
+from keras.models import model_from_json
 
 class VideoCamera(object):
     def __init__(self):
-        # Using OpenCV to capture from device 0.
         self.video = cv2.VideoCapture(0)
-        
-        # Load Haarcascade File
-        self.face_detector = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
-
-        # Load the Model and Weights
+        self.face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
         self.model = model_from_json(open("ml_folder/facial_expression_model_structure.json", "r").read())
         self.model.load_weights('ml_folder/facial_expression_model_weights.h5')
     
@@ -23,9 +16,6 @@ class VideoCamera(object):
         success, frame = self.video.read()
         
         if success:
-            # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-            # so we must encode it into JPEG in order to correctly display the
-            # video stream.
             frame = classifier.classify(frame, self.face_detector, self.model)
-            ret, jpeg = cv2.imencode('.jpg', frame)
+            _, jpeg = cv2.imencode('.jpg', frame)
             return jpeg.tobytes()
--- classifier.py
+++ classifier.py
@@ -1,45 +1,34 @@
 
-from keras.preprocessing import image as im
-
 import cv2
-import json
 import numpy as np
+from keras.preprocessing import image as im
 
 def classify(frame, face_detector, model):
 
-    # Emotions
     emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
-
     gray = frame
-
-    # Detect faces
     detected_faces = face_detector.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=10, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
-
-    # Face properties
     face_prop = []
 
-    # Faces are detected more than 0 i.e not 0
     if len(detected_faces) > 0:
-        # x,y = x,y coordinates
-        # w,h = width, height
+
         for (x, y, w, h) in detected_faces:
             frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
             img = cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-            adjust_img = img[y:y+h, x:x+w]  # Crop img to the face
-            adjust_img = cv2.resize(adjust_img, (48, 48))  # Resize img to fit the ML model
+            adjust_img = img[y:y+h, x:x+w]
+            adjust_img = cv2.resize(adjust_img, (48, 48))
 
             img_tensor = im.img_to_array(adjust_img)
             img_tensor = np.expand_dims(img_tensor, axis=0)
 
-            img_tensor /= 255  # pixels are in scale of [0, 255]. normalize all pixels in scale of [0, 1]
+            img_tensor /= 255
 
-            predictions = model.predict(img_tensor)  # store probabilities of 2 facial expressions
-            label = emotions[np.argmax(predictions)]  # Get label with most probability
+            predictions = model.predict(img_tensor)
+            label = emotions[np.argmax(predictions)]
 
-            confidence = np.max(predictions)  # Get the confidence of that label
-
-            confidence *= 100 # Multiple probability by 100
+            confidence = np.max(predictions)
+            confidence *= 100
 
             detect = dict()
             detect['label'] = label
@@ -56,6 +45,5 @@ def classify(frame, face_detector, model):
         
     cv2.imwrite("somefile.jpeg", frame)
 
-    # output_json = json.dumps([face.__dict__ for face in face_prop])
     return face_prop
 
